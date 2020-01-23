ae4eba38f122ada8866987f4611b927bd217c928
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-01-23 17:40:22 +0300

    feat: add solution and unit tests for Find maximum recursively task.

--- ArrayManipulation/ArrayExtension.cs
+++ ArrayManipulation/ArrayExtension.cs
@@ -5,9 +5,9 @@ namespace ArrayManipulation
     /// <summary>Provides methods for array extension.</summary>
     public static class ArrayExtension
     {
-        /// <summary>Finds the index of the balance.</summary>
+        /// <summary>Finds the maxIndex of the balance.</summary>
         /// <param name="array">An array.</param>
-        /// <returns>Returns index or <em>null</em> if index not found.</returns>
+        /// <returns>Returns maxIndex or <em>null</em> if maxIndex not found.</returns>
         /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
         public static int? FindBalanceIndex(int[] array)
         {
@@ -37,6 +37,42 @@ namespace ArrayManipulation
             }
 
             return null;
+        }
+
+        /// <summary>Finds the maximum item in the given array.</summary>
+        /// <param name="array">An array.</param>
+        /// <returns>Returns a value of maximum element.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
+        public static int FindMaximumItem(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be empty.");
+            }
+
+            return MaximumItem(array, 0, array.Length - 1);
+        }
+
+        private static int MaximumItem(int[] array, int leftIndex, int rightIndex)
+        {
+            int middleIndex;
+            middleIndex = (leftIndex + rightIndex) / 2;
+            int leftPartMax;
+            int rightPartMax;
+            if (leftIndex == rightIndex)
+            {
+                return array[leftIndex];
+            }
+
+            leftPartMax = MaximumItem(array, leftIndex, middleIndex);
+            rightPartMax = MaximumItem(array, middleIndex + 1, rightIndex);
+            return leftPartMax > rightPartMax ? leftPartMax : rightPartMax;
         }
     }
 }
